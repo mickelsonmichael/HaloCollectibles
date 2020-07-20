7400f77067270374bfcea178fae915d59aa607b9
mickelsonmichael <mickelsonmichael@gmail.com>
2020-07-20 16:31:08 -0600

    Update Reach component

--- halo-collectibles/src/features/reach/Reach.jsx
+++ halo-collectibles/src/features/reach/Reach.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Data from "../reach/data.json";
+import Achievements from "../../common/Achievements";
+import TabSection from "../../common/TabSection";
 
-const Reach = (props) => {
-  return <p>Halo: Reach</p>;
+const Reach = () => {
+  const tabs = Data.categories.map(({ achievements, title }) => {
+    return {
+      title,
+      content: <Achievements achievements={achievements} title={title} />,
+    };
+  });
+
+  return (
+    <div>
+      <h2>Halo: Reach</h2>
+
+      <TabSection tabs={tabs} />
+    </div>
+  );
 };
 
 export default Reach;
