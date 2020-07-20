ff9becbd16c40ced8e6e6a232bf6edd731b19055
mickelsonmichael <mickelsonmichael@gmail.com>
2020-07-20 09:14:59 -0600

    Configure basic site navigation
    Add the NavMenu component as the header to the site and configure basic
    react-routing setup to navigate between the Home page and the Reach
    page.

--- halo-collectibles/src/App.js
+++ halo-collectibles/src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Reach from "./features/reach/Reach";
+import NavMenu from "./common/NavMenu";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./common/Index";
+import { Container } from "reactstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <NavMenu />
+        </header>
+        <Container fluid className="mt-3">
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/reach" exact>
+            <Reach />
+          </Route>
+        </Container>
+      </div>
+    </BrowserRouter>
   );
 }
 
