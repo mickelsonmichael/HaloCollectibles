98e3a2b6a02c1164c62c4bc345d11e19359b2337
mickelsonmichael <mickelsonmichael@gmail.com>
2020-07-20 09:14:13 -0600

    Add NavMenu component

---
+++ halo-collectibles/src/common/NavMenu.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+
+const NavMenu = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" dark expand="md">
+        <Link component={NavbarBrand} to="/">
+          Halo Collectibles
+        </Link>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <Link component={NavLink} to="/reach">
+                Reach
+              </Link>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavMenu;
