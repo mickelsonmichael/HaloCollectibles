2838a63b4089ca2823ab7399ce53cdcd03abece0
mickelsonmichael <mickelsonmichael@gmail.com>
2020-07-21 08:41:25 -0600

    Add Halo3 Component

---
+++ halo-collectibles/src/features/halo3/Halo3.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Achievements from "./achievements.json";
+
+const Halo3 = () => {
+  console.log(Achievements);
+  return <p>Halo 3</p>;
+};
+
+export default Halo3;
