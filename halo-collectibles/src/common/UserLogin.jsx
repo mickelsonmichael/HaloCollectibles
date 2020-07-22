98b88eca0175b0c622dffe5be99ec70dafb75e9a
mickelsonmichael <mickelsonmichael@gmail.com>
2020-07-22 12:15:58 -0600

    Add UserLogin form

---
+++ halo-collectibles/src/common/UserLogin.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import {
+  setAchievements,
+  getGamertag,
+  clearSession,
+} from "../utilities/storage";
+import { Input, Button, InputGroup, InputGroupAddon } from "reactstrap";
+
+export default () => {
+  const [gamertag, setGamertag] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [currentGamertag, setCurrentGamertag] = React.useState(getGamertag());
+
+  const getUser = () => {
+    return fetch(
+      "https://halocollectiblesapi.azurewebsites.net/api/Search?name=renson"
+    ).then((response) => response.text());
+  };
+
+  const getProgress = (xuid) => {
+    return fetch(
+      "https://halocollectiblesapi.azurewebsites.net/api/GetHalo?xuid=" + xuid
+    ).then((response) => response.json());
+  };
+
+  const getAchievements = () => {
+    setIsLoading(true);
+
+    getUser()
+      .then((xuid) => getProgress(xuid))
+      .then((achievements) => {
+        setAchievements(gamertag, achievements);
+        setIsLoading(false);
+        setCurrentGamertag(gamertag);
+      });
+  };
+
+  const clearAchievements = () => {
+    clearSession();
+    setCurrentGamertag("");
+  };
+
+  if (currentGamertag) {
+    return (
+      <div>
+        <span className="mr-2">{currentGamertag}</span>
+        <Button
+          size="sm"
+          color="outline-danger"
+          type="button"
+          onClick={() => clearAchievements()}
+        >
+          Clear
+        </Button>
+      </div>
+    );
+  }
+
+  return (
+    <InputGroup>
+      <Input
+        type="text"
+        size="sm"
+        placeholder="Gamertag"
+        onChange={(e) => setGamertag(e.target.value)}
+      />
+      <InputGroupAddon>
+        <Button
+          type="button"
+          color="outline-info"
+          size="sm"
+          onClick={() => getAchievements()}
+          disabled={isLoading}
+        >
+          {isLoading && "Going"}
+          {!isLoading && "Go"}
+        </Button>
+      </InputGroupAddon>
+    </InputGroup>
+  );
+};
