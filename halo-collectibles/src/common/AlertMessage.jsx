9ba95d920655f605df9a6e90d5df5db4f43d05b3
mickelsonmichael <mickelsonmichael@gmail.com>
2020-07-26 19:56:43 -0600

    Add default value to isVisible field

--- halo-collectibles/src/common/AlertMessage.jsx
+++ halo-collectibles/src/common/AlertMessage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Alert } from "reactstrap";
 
-export default ({ children, isVisible, color }) => {
+export default ({ children, isVisible = true, color }) => {
   if (isVisible) {
     return <Alert color={color}>{children}</Alert>;
   } else {
