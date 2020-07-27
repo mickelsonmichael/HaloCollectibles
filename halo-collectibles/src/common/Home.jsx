6b95399bd6ad5b30e2d9d127baee3c1ba51b6691
mickelsonmichael <mickelsonmichael@gmail.com>
2020-07-27 15:20:22 -0600

    Add a helpful alert message when logged in

--- halo-collectibles/src/common/Home.jsx
+++ halo-collectibles/src/common/Home.jsx
@@ -1,17 +1,31 @@
-import React from "react";
-import { Jumbotron } from "reactstrap";
+import React, { useContext } from "react";
+import { Jumbotron, Alert } from "reactstrap";
+import { UserContext } from "../UserContext";
+import AlertMessage from "./AlertMessage";
 
-export default () => (
-  <Jumbotron>
-    <h1 className="display-3">Halo Collectibles</h1>
-    <p className="lead">
-      This site will work as a tracking mechanism for following which
-      achievements and Collectibles you have claimed.
-    </p>
-    <hr className="my-2" />
-    <p>
-      Use the navigation above to select a game and enter your gamertag above to
-      filter the achievements you already have completed.
-    </p>
-  </Jumbotron>
-);
+export default () => {
+  const { gamertag } = useContext(UserContext).user;
+
+  return (
+    <div>
+      <AlertMessage isVisible={gamertag}>
+        You are logged in! Select a game from the navigation above to view the
+        remaining achievements. Or click your name in the corner to view all
+        your completed achievements.
+      </AlertMessage>
+      <Jumbotron>
+        <h1 className="display-3">Halo Collectibles</h1>
+        <p className="lead">
+          This site will work as a tracking mechanism for following which
+          achievements and Collectibles you have claimed.
+        </p>
+        <hr className="my-2" />
+        <p>
+          You may view the achievements using the navigation options above. For
+          a more curated experience, use the textbox to enter your Xbox Live
+          Gamertag and filter out the achievements you already have completed.
+        </p>
+      </Jumbotron>
+    </div>
+  );
+};
