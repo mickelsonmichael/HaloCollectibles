45383ac54cebdaafb6e5a2a2c867c091ca671d94
mickelsonmichael <mickelsonmichael@gmail.com>
2020-07-27 09:00:22 -0600

    Add Achievement Card

---
+++ halo-collectibles/src/common/achievements/AchievementCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import AchievementProgress from "../AchievementProgress";
+import { MdCheckCircle } from "react-icons/md";
+import "./achievements.css";
+
+export default ({ achievement }) => {
+  return (
+    <div className="achievement-card">
+      <div className="achievement-card__title">{achievement.name}</div>
+      <span className="achievement-card__score">
+        {achievement.isComplete && <MdCheckCircle size={15} />}
+        {achievement.score}
+      </span>
+      <div className="achievement-card__description">
+        {achievement.description}
+      </div>
+      <div className="achievement-card__progress">
+        <AchievementProgress achievement={achievement} />
+      </div>
+    </div>
+  );
+};
