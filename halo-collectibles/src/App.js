d406efd2cd34b9f1c89e3572ed6e386af706cc99
mickelsonmichael <mickelsonmichael@gmail.com>
2020-07-26 19:59:58 -0600

    Wrap app in UserContext

--- halo-collectibles/src/App.js
+++ halo-collectibles/src/App.js
@@ -11,37 +11,46 @@ import "./bootstrap.min.css";
 import "./App.css";
 import HaloCE from "./features/haloce/HaloCE";
 import UserAchievements from "./features/user/UserAchievements";
+import UserContext from "./UserContext";
 
 function App() {
+  let [user, setUser] = React.useState({
+    gamertag: "",
+    achievements: [],
+    showComplete: false,
+  });
+
   return (
     <HashRouter>
-      <div className="App">
-        <header>
-          <NavMenu />
-        </header>
-        <Container fluid className="mt-3">
-          <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route path="/user" exact>
-              <UserAchievements />
-            </Route>
-            <Route path="/haloce" exact>
-              <HaloCE />
-            </Route>
-            <Route path="/halo2" exact>
-              <Halo2 />
-            </Route>
-            <Route path="/halo3" exact>
-              <Halo3 />
-            </Route>
-            <Route path="/reach" exact>
-              <Reach />
-            </Route>
-          </Switch>
-        </Container>
-      </div>
+      <UserContext.Provider value={{ user, setUser }}>
+        <div className="App">
+          <header>
+            <NavMenu />
+          </header>
+          <Container fluid className="mt-3">
+            <Switch>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+              <Route path="/user" exact>
+                <UserAchievements />
+              </Route>
+              <Route path="/haloce" exact>
+                <HaloCE />
+              </Route>
+              <Route path="/halo2" exact>
+                <Halo2 />
+              </Route>
+              <Route path="/halo3" exact>
+                <Halo3 />
+              </Route>
+              <Route path="/reach" exact>
+                <Reach />
+              </Route>
+            </Switch>
+          </Container>
+        </div>
+      </UserContext.Provider>
     </HashRouter>
   );
 }
