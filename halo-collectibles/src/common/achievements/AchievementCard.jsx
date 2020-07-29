325c2eebd157daf5775bc7d51128a86c2eefd8a9
mickelsonmichael <mickelsonmichael@gmail.com>
2020-07-29 16:29:37 -0600

    Fix progress bar positioning

--- halo-collectibles/src/common/achievements/AchievementCard.jsx
+++ halo-collectibles/src/common/achievements/AchievementCard.jsx
@@ -6,11 +6,13 @@ import "./achievements.css";
 export default ({ achievement }) => {
   return (
     <div className="achievement-card">
-      <div className="achievement-card__title">{achievement.name}</div>
-      <span className="achievement-card__score">
-        {achievement.isComplete && <MdCheckCircle size={15} />}
-        {achievement.score}
-      </span>
+      <div className="achievement-card__header">
+        <div className="achievement-card__title">{achievement.name}</div>
+        <span className="achievement-card__score">
+          {achievement.isComplete && <MdCheckCircle size={15} />}
+          {achievement.score}
+        </span>
+      </div>
       <div className="achievement-card__description">
         {achievement.description}
       </div>
