11ae43a5c6e603d94f7f44f5ccbc48cc7c810a46
mickelsonmichael <mickelsonmichael@gmail.com>
2020-07-31 13:41:19 -0600

    Add Skulls to Halo 2

--- halo-collectibles/src/features/halo2/Halo2.jsx
+++ halo-collectibles/src/features/halo2/Halo2.jsx
@@ -2,16 +2,38 @@ import React from "react";
 import Achievements from "../../common/achievements/Achievements";
 import Categories from "./achievements.json";
 import AchievementCount from "../../common/achievements/AchievementCount";
+import Skulls from "../../common/skulls/Skulls";
+import skulls from "./skulls.json";
+import { Route, useRouteMatch } from "react-router";
+import { Link } from "react-router-dom";
+import { Nav, NavItem, Navbar, NavbarBrand } from "reactstrap";
+import NavLink from "../../common/layout/NavLink";
 
 const Halo2 = () => {
+  let match = useRouteMatch();
+
   return (
     <div>
-      <h2>
-        Halo 2
-        <AchievementCount className="float-right" categories={Categories} />
-      </h2>
+      <Navbar color="primary" dark expand={false}>
+        <NavbarBrand>Halo 2</NavbarBrand>
+        <Nav navbar className="mr-auto flex-row">
+          <NavLink url={match.path} text="Achievements" />
+          <NavLink url={`${match.path}/skulls`} text="Skulls" />
+        </Nav>
+      </Navbar>
 
-      <Achievements categories={Categories} />
+      <br />
+
+      <Route path={match.path} exact>
+        <h2>
+          Achievements
+          <AchievementCount className="float-right" categories={Categories} />
+        </h2>
+        <Achievements categories={Categories} />
+      </Route>
+      <Route path={`${match.path}/skulls`}>
+        <Skulls skulls={skulls} />
+      </Route>
     </div>
   );
 };
