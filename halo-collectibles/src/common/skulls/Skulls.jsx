83265ddfc1fe223b30e86536b5c243cadccda9a1
mickelsonmichael <mickelsonmichael@gmail.com>
2020-07-31 13:37:29 -0600

    Add components for Skulls

---
+++ halo-collectibles/src/common/skulls/Skulls.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import SkullCard from "./SkullCard";
+import { UserContext } from "../../UserContext";
+import "./skulls.css";
+import { Col, Row, Alert } from "reactstrap";
+
+export default ({ skulls }) => {
+  const { achievements, showComplete } = React.useContext(UserContext).user;
+  let filteredSkulls = skulls;
+
+  if (achievements && achievements.length > 0) {
+    filteredSkulls = filteredSkulls.map((skull) => {
+      const achievement = achievements.find((a) => a.name.includes(skull.name));
+
+      return {
+        ...skull,
+        isFound: achievement ? achievement.isComplete : false,
+      };
+    });
+  }
+
+  if (!showComplete) {
+    filteredSkulls = filteredSkulls.filter((skull) => !skull.isFound);
+  }
+
+  if (filteredSkulls.length === 0) {
+    return (
+      <div>
+        <Alert color="info">
+          You have already unlocked all the skulls! Toggle the switch in the
+          navigation to show your already achieved skulls.
+        </Alert>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>Skulls</h2>
+      <Row noGutters>
+        {filteredSkulls.map((skull) => (
+          <Col sm={12} md={6} lg={4}>
+            <SkullCard skull={skull} key={skull.name} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
