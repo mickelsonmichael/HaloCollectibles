a56fc300f112ccd78621f88f7e2382cbb1f856c5
mickelsonmichael <mickelsonmichael@gmail.com>
2020-08-18 17:37:15 -0600

    Add Halo: CE Terminals

--- halo-collectibles/src/features/haloce/HaloCE.jsx
+++ halo-collectibles/src/features/haloce/HaloCE.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import Game from "../../common/Game";
 import Categories from "./achievements.json";
 import Skulls from "./skulls.json";
+import Terminals from "./terminals.json";
 
 export default () => (
-  <Game achievements={Categories} skulls={Skulls} name="Halo: CE" />
+  <Game
+    achievements={Categories}
+    skulls={Skulls}
+    terminals={Terminals}
+    name="Halo: CE"
+  />
 );
