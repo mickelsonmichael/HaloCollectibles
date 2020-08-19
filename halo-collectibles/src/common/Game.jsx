8667e0dcfa5504d4d1ef6c99d9cab4c3ce283f88
mickelsonmichael <mickelsonmichael@gmail.com>
2020-08-19 10:35:00 -0600

    Switch sidebar to tabs

--- halo-collectibles/src/common/Game.jsx
+++ halo-collectibles/src/common/Game.jsx
@@ -5,40 +5,50 @@ import Terminals from "./terminals/Terminals";
 import { FaSkull, FaTrophy, FaTerminal } from "react-icons/fa";
 
 export default ({ name, achievements, skulls, terminals }) => {
-  const [activeTab, setActiveTab] = React.useState("achievements");
+  const [activeTab, setActiveTab] = React.useState("Achievements");
 
   return (
     <div className="game">
-      <div className="game__sidebar">
-        <ul className="game__sidebar-list">
-          <li className="game__sidebar-list-item">
-            <button
-              onClick={() => setActiveTab("achievements")}
-              className={activeTab === "achievements" ? "active" : ""}
-              title="Achievements"
-            >
-              <FaTrophy />
+      <h2>
+        {name} {activeTab}
+      </h2>
+      <br />
+      <div className="game__navbar">
+        <ul className="game__navbar-list">
+          <li
+            className={
+              "game__navbar-list-item" +
+              (activeTab === "Achievements" ? " active" : "")
+            }
+          >
+            <button onClick={() => setActiveTab("Achievements")}>
+              <FaTrophy /> Achievements
             </button>
           </li>
           {skulls && (
-            <li className="game__sidebar-list-item">
-              <button
-                onClick={() => setActiveTab("skulls")}
-                className={activeTab === "skulls" ? "active" : ""}
-                title="Skulls"
-              >
-                <FaSkull />
+            <li
+              className={
+                "game__navbar-list-item" +
+                (activeTab === "Skulls" ? " active" : "")
+              }
+            >
+              <button onClick={() => setActiveTab("Skulls")}>
+                <FaSkull /> Skulls
               </button>
             </li>
           )}
           {terminals && (
-            <li className="game__sidebar-list-item">
+            <li
+              className={
+                "game__navbar-list-item" +
+                (activeTab === "Terminals" ? " active" : "")
+              }
+            >
               <button
-                onClick={() => setActiveTab("terminals")}
-                className={activeTab === "terminals" ? "active" : ""}
+                onClick={() => setActiveTab("Terminals")}
                 title="Terminals"
               >
-                <FaTerminal />
+                <FaTerminal /> Terminals
               </button>
             </li>
           )}
@@ -47,29 +57,26 @@ export default ({ name, achievements, skulls, terminals }) => {
       <div className="game__sections">
         <div
           className={
-            "game__section" + (activeTab === "achievements" ? " active" : "")
+            "game__section" + (activeTab === "Achievements" ? " active" : "")
           }
         >
-          <h2>{name} Achievements</h2>
           <Achievements categories={achievements} />
         </div>
         {skulls && (
           <div
             className={
-              "game__section" + (activeTab === "skulls" ? " active" : "")
+              "game__section" + (activeTab === "Skulls" ? " active" : "")
             }
           >
-            <h2>{name} Skulls</h2>
             <Skulls skulls={skulls} />
           </div>
         )}
         {terminals && (
           <div
             className={
-              "game__section" + (activeTab === "terminals" ? " active" : "")
+              "game__section" + (activeTab === "Terminals" ? " active" : "")
             }
           >
-            <h2>{name} Terminals</h2>
             <Terminals terminals={terminals} />
           </div>
         )}
