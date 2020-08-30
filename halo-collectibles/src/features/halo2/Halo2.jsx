c38f013072615fe6285cb898766730a627c04ebd
mickelsonmichael <mickelsonmichael@gmail.com>
2020-08-30 11:59:16 -0600

    Halo2: Add terminals

--- halo-collectibles/src/features/halo2/Halo2.jsx
+++ halo-collectibles/src/features/halo2/Halo2.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import Categories from "./achievements.json";
 import skulls from "./skulls.json";
 import Game from "../../common/Game";
+import Halo2Terminals from "./Halo2Terminals.json";
 
 const Halo2 = () => (
-  <Game name="Halo 2" skulls={skulls} achievements={Categories} />
+  <Game
+    name="Halo 2"
+    skulls={skulls}
+    achievements={Categories}
+    terminals={Halo2Terminals}
+  />
 );
 
 export default Halo2;
