7f0c0407f7b832db2c8e45aaada3591ec4bc0d7d
mickelsonmichael <mickelsonmichael@gmail.com>
2020-09-17 12:39:44 -0600

    Remove reference to an unused dependency

--- halo-collectibles/src/common/achievements/Categories.jsx
+++ halo-collectibles/src/common/achievements/Categories.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import FilterList from "../Filter/FilterList";
-import "react-dropdown/style.css";
 
 export default ({ onOptionChange, categories }) => {
   const options = categories.map((c) => ({ value: c.title, text: c.title }));
