40bfcfa16d957c0b5091912570185b119bca9785
mickelsonmichael <mickelsonmichael@gmail.com>
2025-05-16 18:42:49 -0600

    feat: Allow users to enter their SteamID instead of login

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,6 +1,49 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { authClient, steamOpenIdProvider } from "@/utilities/auth";
 import { waitFor } from "@/utilities/delay";
+
+const getTestUrl = (steamId: string) =>
+    `http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/
+        ?appid=976730
+        &key=${process.env.STEAM_API_KEY}
+        &steamid=${steamId}
+        &l=en-US`.replaceAll(/\s/g, "");
+
+export const POST = async (request: NextRequest) => {
+    const sourceUrl = new URL(request.url);
+    const hostUrl = `${sourceUrl.protocol}//${sourceUrl.host}`
+    const errorUrl = `${hostUrl}/error`;
+
+    const form = await request.formData();
+    const steamId = form.get("steamId")?.toString().trim();
+
+    if (steamId == null || steamId === "") {
+        return NextResponse.redirect(`${errorUrl}?message=You must enter a valid SteamID`);
+    }
+
+    const testUrl = getTestUrl(steamId);
+
+    console.log(`Sending test request for SteamId ${steamId} to '${testUrl}'`)
+
+    const steamIdTestResponse = await fetch(testUrl, { cache: "no-cache" });
+
+    if (!steamIdTestResponse.ok) {
+        console.debug(`SteamID authentication failed; received ${steamIdTestResponse.status} ${steamIdTestResponse.statusText}`);
+
+        return NextResponse.redirect(`${errorUrl}?message=You entered an invalid SteamID or your steam profile is private. Please double-check and try again`);
+    }
+
+    const redirectUrl = `${hostUrl}/achievements`;
+
+    const response = NextResponse.redirect(redirectUrl);
+
+    response.cookies.set("STEAM_USER_ID", steamId, {
+        secure: process.env.NODE_ENV === "production",
+        sameSite: true
+    });
+
+    return response;
+}
 
 export const GET = async () => {
 
