3e45abb5f2f48de6706a9d9aa8d6fe0adcc7649f
mickelsonmichael <mickelsonmichael@gmail.com>
2025-05-22 13:13:07 -0600

    fix: Add missing component

---
+++ src/components/achievements/AchievementsTableRow.tsx
@@ -0,0 +1,34 @@
+import { useLogin } from "@/hooks/LoginContext";
+import AchievementWithProgress from "@/models/AchievementWithProgress";
+import ProgressBar from "../ProgressBar";
+
+const AchievementsTableRow = ({
+  achievement,
+}: {
+  achievement: AchievementWithProgress;
+}) => {
+  const { isLoggedInWithXbox } = useLogin();
+
+  const AchievementProgress = () =>
+    achievement.progress !== null &&
+    typeof achievement.progress !== "boolean" &&
+    achievement.progress.target > 1 ? (
+      <td className="w-100">
+        <ProgressBar {...achievement.progress} />
+      </td>
+    ) : (
+      <td />
+    );
+
+  return (
+    <tr className="flex flex-col md:table-row *:p-2 even:bg-blue-500/25">
+      <td className="col-sm">{achievement.name}</td>
+      <td className="w-100">{achievement.description}</td>
+      {isLoggedInWithXbox && <AchievementProgress />}
+      <td className="col-sm">{achievement.game}</td>
+      <td className="col-sm">{achievement.collection}</td>
+    </tr>
+  );
+};
+
+export default AchievementsTableRow;
